39dcb446825927a69356ce42aa976336f5cbb243
prateekvarma <prtk.varma@gmail.com>
2023-03-13 16:18:37 -0600

    Parsing logic to extract data from <form>

--- src/routes/loginRoutes.ts
+++ src/routes/loginRoutes.ts
@@ -2,7 +2,7 @@ import { Router, Request, Response } from 'express';
 
 const router = Router();
 
-router.get('/login', (req: Request, res: Response) => { 
+router.get('/login', (req: Request, res: Response) => {
   res.send(`
     <form method="POST">
       <div>
@@ -16,6 +16,11 @@ router.get('/login', (req: Request, res: Response) => {
       <button>Submit</button>
     </form>
   `);
+});
+
+router.post('/login', (req: Request, res: Response) => {
+  const { email, password } = req.body;
+  res.send(email + ' ' + password);
 });
 
 export { router };
